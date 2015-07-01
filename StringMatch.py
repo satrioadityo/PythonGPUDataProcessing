6676ea26a487b3bb57c23e53fc049d9edd22b6cc
mwpc <mwpc@gmail.com>
2015-07-01 11:43:55 +0700

    oprek pas baca line masukin ke device

--- StringMatch.py
+++ StringMatch.py
@@ -1,11 +1,6 @@
 from timeit import default_timer as timer
 import numpy
-from numbapro import vectorize, float64, cuda
-
-
-@vectorize([float64(float64, float64)], target='gpu')
-def vector_mul(a, b):
-    return  a * b
+from numbapro import vectorize, float64, void
 
 @vectorize([float64(float64, float64)], target='gpu')
 def vector_comp(a, b):
@@ -14,9 +9,21 @@ def vector_comp(a, b):
     else :
         return 0
 
+@vectorize([void(float64[:], float64[:])], target='gpu')
+def vector_comp2(a, b):
+    match = 0
+    for i in range(len(a)):
+        for j in range(len(b)):
+            #print list_master[i],"master"
+            #print list_ref[j],"ref"
+            if a[i] == b[j] :
+                match = a[i]
+                print match
 
-f = open('acc_master.csv','r')
-f2 = open('acc_ref.csv','r')
+#f = open('acc_master.csv','r')
+#f2 = open('acc_ref.csv','r')
+f = open('acc_master_test.txt','r')
+f2 = open('acc_ref_test.txt','r')
 
 list_master = []
 list_ref = []
@@ -36,13 +43,20 @@ print 'hasil compare tiap element'
 count = 0
 match = []
 
-for i in range(len(list_master)):
-    for j in range(len(list_ref)):
-        print list_master[i],"master"
-        print list_ref[j],"ref"
-        aaa = vector_comp(list_master[i], list_ref[j])
-        if aaa != 0 :
-            match.append(str(aaa))
+timeStart = timer()
+
+vector_comp2(list_master,list_ref)
+
+#for i in range(len(list_master)):
+#    for j in range(len(list_ref)):
+        #print list_master[i],"master"
+        #print list_ref[j],"ref"
+#        aaa = vector_comp(list_master[i], list_ref[j])
+#        if aaa != 0 :
+#            match.append(str(list_master[i]))
+timeFinish = timer()
 
 for i in range(len(match)):
     print str(match[i])
+
+print 'execution time = ', timeFinish - timeStart
