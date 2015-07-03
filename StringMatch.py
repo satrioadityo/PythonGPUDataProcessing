c3661bfb03c0290768dc3206cc861839ac409aaf
mwpc <mwpc@gmail.com>
2015-07-03 13:08:43 +0700

    fixed running pake gpu/cpu

--- StringMatch.py
+++ StringMatch.py
@@ -3,15 +3,6 @@ import numpy as np
 import sys
 from numbapro import vectorize, float64, float32, void, cuda
 from numba import *
-import csv
-
-@cuda.jit(argtypes=[f8[:], f8[:], f8[:]], target='gpu')
-def cuda_compare(a, b, c):
-    i = cuda.grid(1)
-    if (a[i] == b[i]) :
-        c[i] = a[i]
-    else :
-        c[i] = 0
 
 @cuda.jit(argtypes=[f8[:], f8[:], f8[:]], target='gpu')
 def cuda_match(a, b, c):
@@ -35,20 +26,13 @@ def cuda_match_cpu(a, b, c) :
 
 griddim = 100, 1
 blockdim = 1024, 1
-N = griddim[0] * blockdim[0]
-M = griddim[1] * blockdim[1]
-print N,M
+
 file1 = raw_input("Nama File 1 : ");
 file2 =	raw_input("Nama File 2 : ");
+runningMethod =	raw_input("Run menggunakan gpu/cpu (g or c) : ");
 
 f = open(file1,'r')
 f2 = open(file2,'r')
-#f = open('acc_master.csv','r')
-#f2 = open('acc_ref.csv','r')
-#f = open('sample1-10000.csv','r')
-#f2 = open('sample2-10000.csv','r')
-#f = open('acc_master_test.csv','r')
-#f2 = open('acc_ref_test.csv','r')
 
 list_master = []
 list_ref = []
@@ -56,12 +40,10 @@ list_ref = []
 loadTime = timer()
 print f
 for line in f:
-	#print line
 	list_master.append(float64(line))
 
 print f2
 for line in f2:
-	#print line
 	list_ref.append(float64(line))
 
 endofLoadTime = timer()
@@ -80,23 +62,25 @@ cc = np.empty_like(aa,dtype=np.float64)
 timeStart = timer() # start count time
 
 #cuda_compare_configured(aa, bb, cc)
-print 'start of process matching in gpu'
+
 stream = cuda.stream()
-
 cc2 = []
-
-for i in range(aa.size/100000) :
-    cc2.append([])
-    with stream.auto_synchronize():
-        cuda_match_configured(aa, bb, cc)
-    cc2[i].append(cc)
-#cuda_match_configured(aa, bb, cc)
-#cuda_match_cpu(aa, bb, cc)
-print 'end of process matching in gpu'
+if runningMethod == 'g' :
+    print 'start of process matching in gpu'
+    for i in range(aa.size/100000) :
+        cc2.append([])
+        with stream.auto_synchronize():
+            cuda_match_configured(aa, bb, cc)
+        cc2[i].append(cc)
+    print 'end of process matching in gpu'
+elif runningMethod == 'c' :
+    print 'start of process matching in cpu'
+    cuda_match_cpu(aa, bb, cc)
+    print 'end of process matching in cpu'
+else :
+    sys.exit
 
 timeFinish = timer() # end count time
-
-#print str(cc)
 
 print 'hasil akhir matching :'
 count = 0
@@ -111,9 +95,10 @@ for i in range(len(cc2)) :
 print 'jumlah data ',file1,' = ',len(list_master)
 print 'jumlah data ',file2,' = ',len(list_ref)
 print 'ada ',count,' data yang match'
-print 'execution time gpu = ', timeFinish - timeStart,' detik'
-#print 'execution time cpu = ', timeFinish - timeStart
-
+if (runningMethod == 'g') :
+    print 'execution time gpu = ', timeFinish - timeStart,' detik'
+elif runningMethod == 'c' :
+    print 'execution time cpu = ', timeFinish - timeStart,' detik'
 
 save = raw_input('simpan id str match ke file? (y/n)')
 if save == 'y' :
