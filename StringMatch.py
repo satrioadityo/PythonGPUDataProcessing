59ddaea7fe41995d6746a313268cef5a7c247984
mwpc <mwpc@gmail.com>
2015-07-03 15:43:46 +0700

    sudah fixed bisa matching berjuta data, dan save ke file

--- StringMatch.py
+++ StringMatch.py
@@ -9,7 +9,7 @@ def cuda_match(a, b, c):
     i = cuda.grid(1)
     for j in range (len(b)) :
         if (a[i] == b[j]) :
-            c[i] = a[i]
+            c[i] = b[j]
             break
         else :
             c[i] = 0
@@ -68,12 +68,15 @@ cc2 = []
 
 if runningMethod == 'g' :
     print 'start of process matching in gpu'
+    p = 0
     for i in range(int(math.ceil(aa.size/100000.0))) :
         cc2.append([])
         with stream.auto_synchronize():
-            cuda_match_configured(aa, bb, cc)
+            cuda_match_configured(aa[p:p+102399], bb, cc)
         cc2[i].append(cc)
+        p+=100000
     print 'end of process matching in gpu'
+    
 elif runningMethod == 'c' :
     print 'start of process matching in cpu'
     cuda_match_cpu(aa, bb, cc)
@@ -85,23 +88,30 @@ timeFinish = timer() # end count time
 
 print 'hasil akhir matching :'
 if (runningMethod == 'g') :
+    print 'execution time gpu = ', timeFinish - timeStart,' detik'
+    for j in range (len(cc2)) :
+        for k in range(len(cc2[j])) :
+            print len(cc2[j][k])
+    
     count = 0
     for i in range(len(cc2)) :
         for j in range(len(cc2[i])) :
-            for k in range(len(cc2[i][j])) :
-            #if cc2[i][j] != 0 :
-                if cc2[i][j][k] != 0 and cc2[i][j][k] > 0 :
-                    #print cc2[i][j][k]
-                    count += 1
+            count += len(cc2[i][j])-cc2[i][j].tolist().count(0)
+
 
     print 'jumlah data ',file1,' = ',len(list_master)
     print 'jumlah data ',file2,' = ',len(list_ref)
     print 'ada ',count,' data yang match'
-    print 'execution time gpu = ', timeFinish - timeStart,' detik'
+    
 elif runningMethod == 'c' :
+    count = 0
+    for i in range(len(cc)) :
+        if cc[i] != 0 and cc[i] > 0 :
+            cc2.append(cc[i])
+            count += 1
     print 'jumlah data ',file1,' = ',len(list_master)
     print 'jumlah data ',file2,' = ',len(list_ref)
-    print 'ada ',len(cc),' data yang match'
+    print 'ada ',count,' data yang match'
     print 'execution time cpu = ', timeFinish - timeStart,' detik'
 
 save = raw_input('simpan id str match ke file? (y/n)')
@@ -118,9 +128,9 @@ if save == 'y' :
         print 'Thanks for using our program'
     elif runningMethod == 'c' :
         fileName = raw_input('nama file ? ')
-        for i in range(len(cc)) :
+        for i in range(len(cc2)) :
             f = open(fileName, 'ab')
-            f.write(str(cc[i])+'\n')
+            f.write(str(cc2[i])+'\n')
         print 'Thanks for using our program'
 else :
     print 'Thanks for using our program'
